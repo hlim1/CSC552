eb9dbd12f9f2f41fbfa4cb12883adb23fac0dda9
Terrence Lim <hlim1@email.arizona.edu>
2019-04-27 14:29:32 -0700

    Fixed errore, which were caused by different fnx. definitions with the installed fuse

--- Project/Phase_1_FUSE/fuse_implement.cpp
+++ Project/Phase_1_FUSE/fuse_implement.cpp
@@ -1,136 +1,146 @@
 #include "fuse_implement.h"
 
-// Fuse_Implement fuse_implement;
+Directory* f_directory;
 
-// Initialize LFS directory layer. This will create a root directory "/".
-static int Fuse_Implement::imp_init(struct fuse_conn_info *conn)
+// Initialize LFS f_directory layer. This will create a root f_directory "/".
+int imp_init(struct fuse_conn_info *conn)
 {
     std::cout << "Initializing fuse in imp_init" << std::endl;
-    return directory = new Directory(((inputState*)fuse_get_context()->private_data)->lfsFile);
-}
-
-static int Fuse_Implement::imp_file_getattr(const char* path, struct stat*)
-{
-    // NOT IMPLEMENTED YET
-    return NOTSUPPORTED;
-}
-
-static int Fuse_Implement::imp_file_open(const char* path, struct fuse_file_info* fi)
-{
-    std::cout << "Opening a file in imp_file_open" << std::endl;
-    return directory->Directory_file_open(path, fi);
-}
-
-static int Fuse_Implement::imp_file_read(const char* path, char* buffer, int length, off_t offset, struct fuse_file_info* fi)
-{
-    std::cout << "File read in imp_file_read" << std::endl;
-    return directory->Directory_file_read(path, buffer, offset, length);
-}
-
-static int Fuse_Implement::imp_file_write(const char* path, void* buffer, off_t offset, int length, struct fuse_file_info* fi)
-{
-    std::cout << "File write in imp_file_write" << std::endl;
-    return directory->Directory_file_write(path, buffer, offset, length);
-}
-
-static int Fuse_Implement::imp_unlink(const char* path)
-{
-    std::cout << "Unlinking a file from the directory. imp_unlink." << std::endl;
-    return unlink(path);
-}
-
-static int Fuse_Implement::imp_rmdir(const char* path)
-{
-    // NOT IMPLEMENTED YET
-    std::cout << "Removing directory. imp_rmdir." << std::endl;
+    f_directory = new Directory();
     return 0;
 }
 
-static int Fuse_Implement::imp_statfs(const char* path, struct statvfs* stbuf)
+int imp_file_getattr(const char* path, struct stat*)
+{
+    return NOTSUPPORTED;
+}
+
+int imp_file_open(const char* path, struct fuse_file_info* fi)
+{
+    std::cout << "Opening a file in imp_file_open" << std::endl;
+    int status = f_directory->Directory_file_open(path, fi);
+    return status;
+}
+
+int imp_file_read(const char* path, char* buffer, size_t length, off_t offset, struct fuse_file_info* fi)
+{
+    std::cout << "File read in imp_file_read" << std::endl;
+    int status = f_directory->Directory_file_read(path, buffer, offset, length);
+    return status;
+}
+int imp_file_write(const char* path, char* buffer, off_t offset, size_t length, struct fuse_file_info* fi)
+{
+    std::cout << "File write in imp_file_write" << std::endl;
+    int status = f_directory->Directory_file_write(path, buffer, offset, length);
+    return status;
+}
+
+int imp_unlink(const char* path)
+{
+    std::cout << "Unlinking a file from the f_directory. imp_unlink." << std::endl;
+    return unlink(path);
+}
+
+int imp_rmdir(const char* path)
+{
+    // NOT IMPLEMENTED YET
+    std::cout << "Removing f_directory. imp_rmdir." << std::endl;
+    return 0;
+}
+
+int imp_statfs(const char* path, struct statvfs* stbuf)
 {
     std::cout << "Return statistics about the filesystem. imp_statfs" << std::endl;
-    return statvfs(path, statvfs);
+    return NOTSUPPORTED;
 }
 
-static int Fuse_Implement::imp_file_release(const char* path, struct fuse_file_info *fi)
+int imp_file_release(const char* path, struct fuse_file_info *fi)
 {
-    std::cout << "Free up any temporarily allocated data structures when done with a file. file_release"
-              << std::endl
-    return directory->Directory_file_free(path, fi);
+    std::cout << "Free up any temporarily allocated data structures when done with a file. file_release" << std::endl;
+    int status = f_directory->Directory_file_free(path);
+    return status;
 }
 
-static int Fuse_Implement::imp_mkdir(const char* path, mode_t mode)
+int imp_mkdir(const char* path, mode_t mode)
 {
     std::cout << "mkdir. imp_mkdir" << std::endl;
-    return directory->Directory_Create(path, mode);
+    int status = f_directory->Directory_create(path, mode);
+    return status;
 }
 
-static int Fuse_Implement::imp_dir_open(const char* path, struct fuse_file_info* fi)
+int imp_dir_open(const char* path, struct fuse_file_info* fi)
 {
-    std::cout << "Open directory. dir_open" << std::endl;
-    return directory->Directory_open(path, fi);
+    std::cout << "Open f_directory. dir_open" << std::endl;
+    int status = f_directory->Directory_open(path, fi);
+    return status;
 }
 
-static int Fuse_Implement::imp_dir_read(const char* path, fuse_fill_dir_t filler, void* buffer, off_t offset, struct fuse_file_info* fi)
+int imp_dir_read(const char* path, fuse_fill_dir_t filler, char* buffer, off_t offset, struct fuse_file_info* fi)
 {
-    std::cout << "Read and return one or more directory entries to the caller. imp_dir_read." << std::endl;
-    return directory->Directory_read(path, length, buffer, filler, offset, fi);
+    std::cout << "Read and return one or more f_directory entries to the caller. imp_dir_read." << std::endl;
+    int status = f_directory->Directory_read(path, buffer, filler, offset, fi);
+    return status;
 }
 
-static int Fuse_Implement::imp_dir_release(const char* path, struct fuse_file_info* fi)
+int imp_dir_release(const char* path, struct fuse_file_info* fi)
 {
-    std::cout << "Free up any temporarily allocated data structures when done with a directory. im_dir_release" << std::endl;
-
-    return directory->Directory_free(path, fi)
+    std::cout << "Free up any temporarily allocated data structures when done with a f_directory. imp_dir_release" << std::endl;
+    int status = f_directory->Directory_free(path);
+    return status;
 }
 
-static int Fuse_Implement::imp_destroy(void *private_data)
+void imp_destroy(void *private_data)
 {
-    std::cout << "Destroy directory" << std::endl;
-    delete dictionary;
+    std::cout << "Destroy f_directory" << std::endl;
+    delete f_directory;
 }
 
 
-static int Fuse_Implement::imp_file_create(const char* path, mode_t mode, struct fuse_file_info* fi)
+int imp_file_create(const char* path, mode_t mode, struct fuse_file_info* fi)
 {
     std::cout << "Create a file. imp_file_create" << std::endl;
-    return directory->Directory_file_create(path, fi);
+    int status = f_directory->Directory_file_create(path, mode, fi);
+    return status;
 }
 
-static int Fuse_Implement::imp_link(const char* src_path, const char* dest_path)
+int imp_link(const char* src_path, const char* dest_path)
 {
     std::cout << "Create a hard link between src_path and dest_path." << std::endl;
     return NOTSUPPORTED;
 }
 
-static int Fuse_Implement::imp_symlink(const char* src_path, const char* dest_path)
+int imp_symlink(const char* src_path, const char* dest_path)
 {
     std::cout << "Create a symbolic link named src_path which, when evaluated, will lead to dest_path"
               << std::endl;
     return NOTSUPPORTED;
 }
 
-static int Fuse_Implement::imp_truncate(const char* path, int size)
+int imp_truncate(const char* path, size_t length)
 {
     std::cout << "Truncate or extend the given file so that it is precisely size bytes long. imp_truncate."
               << std::endl;
-    return dictionary->Directory_file_truncate(path, size);
+    int status = f_directory->Directory_file_truncate(path, length);
+    return status;
 }
 
-static int Fuse_Implement::imp_rename(const char* org_path, const char* new_path, unsigned int flags)
+int imp_rename(const char* org_path, const char* new_path)
 {
     std::cout << "Rename a file. imp_rename" << std::endl;
-    return Directory->Directory_file_rename(org_path, new_path);
+    int status = f_directory->Directory_file_rename(org_path, new_path);
+    return status;
 }
 
-static int Fuse_Implement::imp_chmod(const char * path, mode_t mode, struct fuse_file_info *fi)
+int imp_chmod(const char * path, mode_t mode)
 {
     std::cout << "Change mode. imp_chmod" << std::endl;
-    return Directory->Directory_chmod(path, mode, fi);
+    int status = f_directory->Directory_chmod(path, mode);
+    return status;
 }
 
-static int Fuse_Implement::imp_chown(const char * path, uid_t uid, gid_t id, struct fuse_file_info *fi)
+int imp_chown(const char * path, uid_t uid, gid_t id)
 {
     std::cout << "Change owner. imp_chown" << std::endl;
-    return Directory->Directory_chown(path, uid, id, fi);
+    int status = f_directory->Directory_chown(path, uid, id);
+    return status;
 }
--- Project/Phase_1_FUSE/fuse_implement.h
+++ Project/Phase_1_FUSE/fuse_implement.h
@@ -20,9 +20,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/statvfs.h>
+#include <fuse.h>
 
 // Class header files list under here:
-#include <fuse.h>
 #include "directory.h"
 
 const int NOTSUPPORTED = -1;
@@ -34,47 +34,47 @@ int imp_file_getattr(const char* path, struct stat*);
 int imp_unlink(const char* path);
 int imp_rmdir(const char* path);
 int imp_file_open(const char* path, struct fuse_file_info* fi);
-int imp_file_read(const char* path, char* buffer, int length, off_t offset, struct fuse_file_info* fi);
-int imp_file_write(const char* path, void* buffer, off_t offset, int length, struct fuse_file_info *fi);
+int imp_file_read(const char* path, char* buffer, size_t length, off_t offset, struct fuse_file_info* fi);
+int imp_file_write(const char* path, char* buffer, off_t offset, size_t length, struct fuse_file_info *fi);
 int imp_statfs(const char* path, struct statvfs* stbuf);
 int imp_file_release(const char* path, struct fuse_file_info *fi);
 int imp_mkdir(const char* path, mode_t mode);
 int imp_dir_open(const char* path, struct fuse_file_info* fi);
-int imp_dir_read(const char* path, fuse_fill_dir_t filler, void* buffer, off_t offset, struct fuse_file_info* fi);
+int imp_dir_read(const char* path, fuse_fill_dir_t filler, char* buffer, off_t offset, struct fuse_file_info* fi);
 int imp_dir_release(const char* path, struct fuse_file_info* fi);
-int imp_destroy(void *private_data);
+void imp_destroy(void *private_data);
 int imp_file_create(const char* path, mode_t mode, struct fuse_file_info* fi);
 int imp_link(const char* src_path, const char* dest_path);
 int imp_symlink(const char* src_path, const char* dest_path);
-int imp_truncate(const char* path, int size);
-int imp_rename(const char* org_path, const char* new_path, unsigned int flags);
-int imp_chmod(const char * path, mode_t mode, struct fuse_file_info *fi);
-int imp_chown(const char * path, uid_t uid, gid_t id, struct fuse_file_info *fi);
+int imp_truncate(const char* path, size_t length);
+int imp_rename(const char* org_path, const char* new_path);
+int imp_chmod(const char * path, mode_t mode);
+int imp_chown(const char * path, uid_t uid, gid_t id);
 
 /*** Edit by Sabin ***/
 //extern Fuse_Implement fuse_implement;
 /*** End edit ***/
 
-static struct fuse_operations fuse_imp_oper = {
-    .init       =   Fuse_Implement::imp_init,
-    .getattr    =   Fuse_Implement::imp_file_getattr,
-    .unlink     =   Fuse_Implement::imp_unlink,
-    .rmdir      =   Fuse_Implement::imp_rmdir,
-    .open       =   Fuse_Implement::imp_file_open,
-    .opendir    =   Fuse_Implement::imp_dir_open,
-    .read       =   Fuse_Implement::imp_file_read,
-    .write      =   Fuse_Implement::imp_file_write,
-    .statfs     =   Fuse_Implement::imp_statfs,
-    .release    =   Fuse_Implement::imp_file_release,
-    .releasedir =   Fuse_Implement::imp_dir_release,
-    .destroy    =   Fuse_Implement::imp_destroy,
-    .create     =   Fuse_Implement::imp_file_create,
-    .link       =   Fuse_Implement::imp_link,
-    .symlink    =   Fuse_Implement::imp_symlink,
-    .truncate   =   Fuse_Implement::imp_truncate,
-    .rename     =   Fuse_Implement::imp_rename,
-    .chmod      =   Fuse_Implement::imp_chmod,
-    .chown      =   Fuse_Implement::imp_chown,
+static struct fuse_operations_comp2 prefix_oper {
+    .init       =   imp_init,
+    .getattr    =   imp_file_getattr,
+    .unlink     =   imp_unlink,
+    .rmdir      =   imp_rmdir,
+    .open       =   imp_file_open,
+    .opendir    =   imp_dir_open,
+    .read       =   imp_file_read,
+    .write      =   imp_file_write,
+    .statfs     =   imp_statfs,
+    .release    =   imp_file_release,
+    .releasedir =   imp_dir_release,
+    .destroy    =   imp_destroy,
+    .create     =   imp_file_create,
+    .link       =   imp_link,
+    .symlink    =   imp_symlink,
+    .truncate   =   imp_truncate,
+    .rename     =   imp_rename,
+    .chmod      =   imp_chmod,
+    .chown      =   imp_chown,
 };
 
 #endif
